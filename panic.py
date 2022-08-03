82137e38262dcd6464c6c72891e1e709513fb7f5
Dmitry <Gravizapin@mail.ru>
2022-08-03 19:15:34 +0300

    testpanic

---
+++ panic.py
@@ -0,0 +1,13 @@
+pharse = "Don't panic!"
+plist = list(pharse)
+print(pharse)
+print(plist)
+for i in range(4):
+    plist.pop()
+plist.pop(0)
+plist.remove("'")
+plist.extend([plist.pop(), plist.pop()])
+plist.insert(2, plist.pop(3))
+new_pharse =' '.join(plist)
+print(plist)
+print(new_pharse)
